30a44df7af78a1ddcdeb6999df7e6dbe69337924
Manakov Arseniy <108518693+c3n9@users.noreply.github.com>
2026-04-23 10:16:09 +0300

    initialize project

---
+++ UnityBuilder/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace UnityBuilder.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
