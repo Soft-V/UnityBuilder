4f08d0a7de6a319113f9052372f48b62c8949d6b
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2026-04-23 10:30:52 +0300

    Build parameters and interface

---
+++ UnityBuilder/Commands/IBuildCommand.cs
@@ -0,0 +1,9 @@
+﻿using UnityBuilder.Models;
+
+namespace UnityBuilder.Commands
+{
+    public interface IBuildCommand
+    {
+        int Build(BuildParameters parameters);
+    }
+}
---
+++ UnityBuilder/Models/BuildParameters.cs
@@ -0,0 +1,11 @@
+﻿namespace UnityBuilder.Models
+{
+    public class BuildParameters
+    {
+        public string UnityPath { get; set; }
+        public string ProjectPath { get; set; }
+        public string OutputPath { get; set; }
+        public string BuildVersion { get; set; }
+        public string TargetPlatform { get; set; }
+    }
+}
