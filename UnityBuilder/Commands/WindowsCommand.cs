c5f66bbccf2554be9ee8d53b2937943f3f860218
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2026-04-23 11:14:48 +0300

    Build shite added

--- UnityBuilder/Commands/WindowsCommand.cs
+++ UnityBuilder/Commands/WindowsCommand.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using UnityBuilder.Models;
 
 namespace UnityBuilder.Commands
@@ -7,7 +9,37 @@ namespace UnityBuilder.Commands
     {
         async public Task<int> Build(BuildParameters parameters)
         {
-            
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                FileName = parameters.UnityPath,
+                Arguments = string.Join(' ', 
+                [
+                    "-quit",
+                    "-batchmode",
+                    "-silent-crashes",
+                    "-projectPath", $"\"{parameters.ProjectPath}\"",
+                    "-executeMethod", "UnityBuilderAction.Builder.BuildProject", // TODO: allow custom
+                    "-customBuildTarget", $"\"{parameters.TargetPlatform}\"",
+                    "-buildTarget", $"\"{parameters.TargetPlatform}\"",
+                    "-customBuildPath", $"\"{parameters.OutputPath}\"",
+                    "-customBuildProfile", "",
+                    "-buildVersion", $"\"{parameters.BuildVersion}\"",
+                    /* TODO: android
+                    "-androidVersionCode", "`"$Env: ANDROID_VERSION_CODE`"",
+                    "-androidKeystorePass", "`"$Env: ANDROID_KEYSTORE_PASS`"",
+                    "-androidKeyaliasName", "`"$Env: ANDROID_KEYALIAS_NAME`"",
+                    "-androidKeyaliasPass", "`"$Env: ANDROID_KEYALIAS_PASS`"",
+                    "-androidTargetSdkVersion", "`"$Env: ANDROID_TARGET_SDK_VERSION`"",
+                    "-androidExportType", "`"$Env: ANDROID_EXPORT_TYPE`"",
+                    "-androidSymbolType", "`"$Env: ANDROID_SYMBOL_TYPE`"",
+                    */
+                    "-logfile", "-"
+                ]),
+                CreateNoWindow = true,
+            };
+            var proc = Process.Start(startInfo);
+            await proc.WaitForExitAsync();
+            return proc.ExitCode;
         }
 
         async public Task<int> ComputeHash(HashParameters parameters)
--- UnityBuilder/Models/BuildParameters.cs
+++ UnityBuilder/Models/BuildParameters.cs
@@ -8,4 +8,13 @@
         public string BuildVersion { get; set; }
         public string TargetPlatform { get; set; }
     }
+
+    // check here https://github.com/game-ci/unity-builder/blob/main/src/model/platform.ts
+    public static class TargetPlatforms
+    {
+        public const string OsX = "StandaloneOSX";
+        public const string Windows = "StandaloneWindows";
+        public const string Windows64 = "StandaloneWindows64";
+        public const string Linux64 = "StandaloneLinux64";
+    }
 }
