acfa153d88fc5bc8463472a77427acbd512772f4
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2026-04-23 13:29:31 +0300

    Console test project

---
+++ UnityBuilder.ConsoleTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using UnityBuilder.Commands;
+using UnityBuilder.Models;
+
+namespace UnityBuilder.ConsoleTest
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            await TestWindowsBuild();
+            Console.WriteLine("Hello, World!");
+        }
+
+        async private static Task TestWindowsBuild()
+        {
+            var cts = new CancellationTokenSource();
+            var buildParameters = new BuildParameters()
+            {
+                BuildVersion = "1.0.0",
+                OutputPath = "C:\\RobocadBuild",
+                ProjectPath = "C:\\Scripts\\robocadV-dev",
+                TargetPlatform = TargetPlatforms.Windows64,
+                UnityPath = "C:\\Program Files\\Unity\\Hub\\Editor\\2021.3.45f2\\Editor\\Unity.exe",
+            };
+
+            var winBuild = new WindowsCommand();
+            var res = await winBuild.Build(buildParameters, cts.Token);
+        }
+    }
+}
