a6ab0fe7b966faf87880a1ce09bcd9e650aa2c0d
Manakov Arseniy <108518693+c3n9@users.noreply.github.com>
2026-04-23 11:18:34 +0300

    Начал верстку + стили

---
+++ UnityBuilder/Converters/AndBoolConverter.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityBuilder.Converters
+{
+    public class AndBoolConverter : IMultiValueConverter
+    {
+        public static readonly AndBoolConverter Instance = new();
+
+        public object Convert(IList<object?> values, Type targetType, object parameter, CultureInfo culture)
+        {
+            foreach (var v in values)
+            {
+                if (v is bool b && !b)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
