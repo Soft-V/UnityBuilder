600121ee98b8c51ed0e33e22c0a22323fcc05cc4
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2026-04-29 11:31:29 +0300

    Build scripts added

---
+++ build-win/build32.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Version
+buildVersion=$(<../build-resources/version.txt)
+
+# Clean-up
+rm -rf ./staging_folder32/
+
+# .NET publish
+# self-contained is recommended, so final users won't need to install .NET
+dotnet publish ../UnityBuilder.Desktop/UnityBuilder.Desktop.csproj --configuration Release --runtime win-x86 --self-contained -f net9.0
+echo "Published"
+
+# Staging directory
+mkdir -p staging_folder32
+
+# Other files
+cp -f -a ../UnityBuilder.Desktop/bin/Release/net9.0/win-x86/publish/. ./staging_folder32/ # copies all files from publish dir
+echo "UnityBuilder copied"
+
+# Make .exe file
+makensis -V1 -DVERSION=$buildVersion ./nsis-setupper32.nsi
+echo ".exe created"
+
---
+++ build-win/buildArm64.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Version
+buildVersion=$(<../build-resources/version.txt)
+
+# Clean-up
+rm -rf ./staging_folder_arm64/
+
+# .NET publish
+# self-contained is recommended, so final users won't need to install .NET
+dotnet publish ../UnityBuilder.Desktop/UnityBuilder.Desktop.csproj --configuration Release --runtime win-arm64 --self-contained -f net9.0
+echo "Published"
+
+# Staging directory
+mkdir staging_folder_arm64
+
+# Other files
+cp -f -a ../UnityBuilder.Desktop/bin/Release/net9.0/win-arm64/publish/. ./staging_folder_arm64/ # copies all files from publish dir
+echo "UnityBuilder copied"
+
+# Make .exe file
+makensis -V1 -DVERSION=$buildVersion ./nsis-setupper-arm64.nsi
+echo ".exe created"
+
