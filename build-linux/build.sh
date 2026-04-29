59a3305f5b0e7769c9471f0c0487c2a765d6a584
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2026-04-29 11:52:20 +0300

    Build changes

--- build-linux/build.sh
+++ build-linux/build.sh
@@ -9,7 +9,7 @@ rm -rf ./staging_folder/
 
 # .NET publish
 # self-contained is recommended, so final users won't need to install .NET
-dotnet publish ../CraftHub/CraftHub.csproj --configuration Release --runtime linux-x64 --self-contained -f net9.0
+dotnet publish ../UnityBuilder.Desktop/UnityBuilder.Desktop.csproj --configuration Release --runtime linux-x64 --self-contained -f net9.0
 echo "Published"
 
 # Staging directory
@@ -24,27 +24,27 @@ echo "Control copied"
 # Starter script
 mkdir -p ./staging_folder/usr
 mkdir -p ./staging_folder/usr/bin
-cp ./linux-data/crafthub ./staging_folder/usr/bin/crafthub
-chmod +x ./staging_folder/usr/bin/crafthub # set executable permissions to starter script
+cp ./linux-data/unity-builder ./staging_folder/usr/bin/unity-builder
+chmod +x ./staging_folder/usr/bin/unity-builder # set executable permissions to starter script
 echo "Started copied"
 
 # Other files
 mkdir -p ./staging_folder/usr/share
-mkdir -p ./staging_folder/usr/share/crafthub
-cp -f -a ../CraftHub/bin/Release/net9.0/linux-x64/publish/. ./staging_folder/usr/share/crafthub/ # copies all files from publish dir
-chmod -R a+rX ./staging_folder/usr/share/crafthub/ # set read permissions to all files
-chmod a+x ./staging_folder/usr/share/crafthub/CraftHub # set executable permissions to main executable
-echo "CraftHub copied"
+mkdir -p ./staging_folder/usr/share/unity-builder
+cp -f -a ../UnityBuilder.Desktop/bin/Release/net9.0/linux-x64/publish/. ./staging_folder/usr/share/unity-builder/ # copies all files from publish dir
+chmod -R a+rX ./staging_folder/usr/share/unity-builder/ # set read permissions to all files
+chmod a+x ./staging_folder/usr/share/unity-builder/UnityBuilder.Desktop # set executable permissions to main executable
+echo "UnityBuilder copied"
 
 # Desktop shortcut
 mkdir -p ./staging_folder/usr/share/applications
-cp ./linux-data/CraftHub.desktop ./staging_folder/usr/share/applications/CraftHub.desktop
+cp ./linux-data/UnityBuilder.desktop ./staging_folder/usr/share/applications/UnityBuilder.desktop
 echo "Shortcut copied"
 
 # Desktop icon
 # A 1024px x 1024px PNG, like VS Code uses for its icon
 mkdir -p ./staging_folder/usr/share/pixmaps
-cp ../build-resources/logo.png ./staging_folder/usr/share/pixmaps/crafthub.png
+cp ../build-resources/logo.png ./staging_folder/usr/share/pixmaps/unity-builder.png
 echo "Icon copied"
 
 # Hicolor icons
@@ -52,17 +52,10 @@ mkdir -p ./staging_folder/usr/share/icons
 mkdir -p ./staging_folder/usr/share/icons/hicolor
 mkdir -p ./staging_folder/usr/share/icons/hicolor/scalable
 mkdir -p ./staging_folder/usr/share/icons/hicolor/scalable/apps
-cp ../build-resources/logo.svg ./staging_folder/usr/share/icons/hicolor/scalable/apps/crafthub.svg
+cp ../build-resources/logo.svg ./staging_folder/usr/share/icons/hicolor/scalable/apps/unity-builder.svg
 echo "Another icon copied"
 
 # Make .deb file
-dpkg-deb --root-owner-group --build ./staging_folder/ ./crafthub_amd64.deb
+dpkg-deb --root-owner-group --build ./staging_folder/ ./unity-builder-amd64.deb
 echo ".deb created"
 
-# Moving files to volumes
-mkdir -p /home/build
-mkdir -p /home/build/inst
-mkdir -p /home/build/full
-yes | cp ./crafthub_amd64.deb /home/build/inst/crafthub_amd64.deb
-yes | cp -f -a ./staging_folder/usr/share/crafthub/. /home/build/full/ # copies all files from publish dir
-echo "All files copied to volumes"
--- build-linux/buildArm64.sh
+++ build-linux/buildArm64.sh
@@ -8,7 +8,7 @@ rm -rf ./staging_folder_arm64/
 
 # .NET publish
 # self-contained is recommended, so final users won't need to install .NET
-dotnet publish ../CraftHub/CraftHub.csproj --configuration Release --runtime linux-arm64 --self-contained -f net9.0
+dotnet publish ../UnityBuilder.Desktop/UnityBuilder.Desktop.csproj --configuration Release --runtime linux-arm64 --self-contained -f net9.0
 echo "Published"
 
 # Staging directory
@@ -23,31 +23,31 @@ echo "Control copied"
 # Starter script
 mkdir -p ./staging_folder_arm64/usr
 mkdir -p ./staging_folder_arm64/usr/bin
-cp ./linux-data-arm64/crafthub ./staging_folder_arm64/usr/bin/crafthub
-chmod +x ./staging_folder_arm64/usr/bin/crafthub # set executable permissions to starter script
+cp ./linux-data-arm64/unity-builder ./staging_folder_arm64/usr/bin/unity-builder
+chmod +x ./staging_folder_arm64/usr/bin/unity-builder # set executable permissions to starter script
 echo "Started copied"
 
 # Other files
 mkdir -p ./staging_folder_arm64/usr/share
-mkdir -p ./staging_folder_arm64/usr/share/crafthub
-cp -f -a ../CraftHub/bin/Release/net9.0/linux-arm64/publish/. ./staging_folder_arm64/usr/share/crafthub/ # copies all files from publish dir
-chmod -R a+rX ./staging_folder_arm64/usr/share/crafthub/ # set read permissions to all files
-chmod a+x ./staging_folder_arm64/usr/share/crafthub/CraftHub # set executable permissions to main executable
-echo "CraftHub copied"
+mkdir -p ./staging_folder_arm64/usr/share/unity-builder
+cp -f -a ../UnityBuilder.Desktop/bin/Release/net9.0/linux-arm64/publish/. ./staging_folder_arm64/usr/share/unity-builder/ # copies all files from publish dir
+chmod -R a+rX ./staging_folder_arm64/usr/share/unity-builder/ # set read permissions to all files
+chmod a+x ./staging_folder_arm64/usr/share/unity-builder/UnityBuilder.Desktop # set executable permissions to main executable
+echo "UnityBuilder copied"
 
 # Desktop shortcut
 mkdir -p ./staging_folder_arm64/usr/share/applications
-cp ./linux-data-arm64/CraftHub.desktop ./staging_folder_arm64/usr/share/applications/CraftHub.desktop
+cp ./linux-data-arm64/UnityBuilder.desktop ./staging_folder_arm64/usr/share/applications/UnityBuilder.desktop
 echo "Shortcut copied"
 
 # Stables
-cp ../build-resources/computed_stables.txt ./staging_folder_arm64/usr/share/crafthub/computed_stables.txt
+cp ../build-resources/computed_stables.txt ./staging_folder_arm64/usr/share/unity-builder/computed_stables.txt
 echo "Stables copied"
 
 # Desktop icon
 # A 1024px x 1024px PNG, like VS Code uses for its icon
 mkdir -p ./staging_folder_arm64/usr/share/pixmaps
-cp ../build-resources/logo.png ./staging_folder_arm64/usr/share/pixmaps/crafthub.png
+cp ../build-resources/logo.png ./staging_folder_arm64/usr/share/pixmaps/unity-builder.png
 echo "Icon copied"
 
 # Hicolor icons
@@ -55,17 +55,9 @@ mkdir -p ./staging_folder_arm64/usr/share/icons
 mkdir -p ./staging_folder_arm64/usr/share/icons/hicolor
 mkdir -p ./staging_folder_arm64/usr/share/icons/hicolor/scalable
 mkdir -p ./staging_folder_arm64/usr/share/icons/hicolor/scalable/apps
-cp ../build-resources/logo.svg ./staging_folder_arm64/usr/share/icons/hicolor/scalable/apps/crafthub.svg
+cp ../build-resources/logo.svg ./staging_folder_arm64/usr/share/icons/hicolor/scalable/apps/unity-builder.svg
 echo "Another icon copied"
 
 # Make .deb file
-dpkg-deb --root-owner-group --build ./staging_folder_arm64/ ./crafthub_arm64.deb
+dpkg-deb --root-owner-group --build ./staging_folder_arm64/ ./unity-builder-arm64.deb
 echo ".deb created"
-
-# Moving files to volumes
-mkdir -p /home/build
-mkdir -p /home/build/instArm64
-mkdir -p /home/build/fullArm64
-yes | cp ./crafthub_arm64.deb /home/build/instArm64/crafthub_arm64.deb
-yes | cp -f -a ./staging_folder_arm64/usr/share/crafthub/. /home/build/fullArm64/ # copies all files from publish dir
-echo "All files copied to volumes"
